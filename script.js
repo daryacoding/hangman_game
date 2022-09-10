007a75d187936631486e79286b95c2c947d67626
daryacoding <abdullahzadeh.darya@gmail.com>
2022-09-10 12:36:28 -0400

    added buttons

--- script.js
+++ script.js
@@ -2,4 +2,55 @@ const words = ['flapjack', 'eraser', 'ginger'];
 
 let randomWord = Math.floor(Math.random() * words.length);
 let word = words[randomWord];
-console.log(word);
+
+const startButton = document.querySelector('.start-game');
+
+startButton.addEventListener('click', (evt) =>{
+    easyMode();
+    mediumMode();
+    hardMode();
+})
+
+function easyMode (){
+    // 1. Create the button
+    const easyButton = document.createElement("button");
+    easyButton.innerHTML = "Easy";
+
+    // 2. Append somewhere
+    const body = document.getElementsByTagName("body")[0];
+    body.appendChild(easyButton);
+
+    // 3. Add event handler
+    easyButton.addEventListener ("click", function() {
+    alert("did something");
+    });
+}
+function mediumMode (){
+    // 1. Create the button
+    const mediumButton = document.createElement("button");
+    mediumButton.innerHTML = "Hard";
+
+    // 2. Append somewhere
+    const body = document.getElementsByTagName("body")[0];
+    body.appendChild(mediumButton);
+
+    // 3. Add event handler
+    mediumButton.addEventListener ("click", function() {
+    alert("did something");
+    });
+}
+
+function hardMode (){
+    // 1. Create the button
+    const hardButton = document.createElement("button");
+    hardButton.innerHTML = "Hard";
+
+    // 2. Append somewhere
+    const body = document.getElementsByTagName("body")[0];
+    body.appendChild(hardButton);
+
+    // 3. Add event handler
+    hardButton.addEventListener ("click", function() {
+    alert("did something");
+    });
+}
